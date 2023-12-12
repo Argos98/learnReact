496e5760c41b7a6646ce16fa00f75109fc04b4e3
David Serrano Gutierrez <davidsegu9@gmail.com>
2023-12-11 20:08:12 -0600

    add event for the search

--- curso-react-intro-main/src/componets/ToDoSearch.js
+++ curso-react-intro-main/src/componets/ToDoSearch.js
@@ -5,10 +5,13 @@ function ToDoSearch() {
   return <input placeholder="nombre de la tarea a buscar"
    className="TodoSearch" 
    onChange={(event) => {
+      //este evento se ejecuta cada vez que se escribe en el input
+      //y me trae el valor de lo que estoy escribiendo en el input
+      console.log('escribiendo', event.target.value);
 
     
    }}
-   />;
-}
+   />
+};
 
 export  {ToDoSearch};
