e46ac3189df43c203a63d1186480ad621c28e8d3
David Serrano Gutierrez <davidsegu9@gmail.com>
2023-12-12 10:15:53 -0600

    add a simple state of react

--- curso-react-intro-main/src/componets/ToDoSearch.js
+++ curso-react-intro-main/src/componets/ToDoSearch.js
@@ -2,16 +2,22 @@ import React from "react";
 import "../css/ToDoSearch.css";
 
 function ToDoSearch() {
-  return <input placeholder="nombre de la tarea a buscar"
-   className="TodoSearch" 
-   onChange={(event) => {
-      //este evento se ejecuta cada vez que se escribe en el input
-      //y me trae el valor de lo que estoy escribiendo en el input
-      console.log('escribiendo', event.target.value);
 
-    
-   }}
+   const [searchValue, setSearchValue] = React.useState('');
+
+   //console.log('1'+searchValue);
+   return <input placeholder="nombre de la tarea a buscar"
+      className="TodoSearch"
+      value={searchValue}
+      onChange={(event) => {
+         //este evento se ejecuta cada vez que se escribe en el input
+         //y me trae el valor de lo que estoy escribiendo en el input
+         //console.log('escribiendo', event.target.value);
+         setSearchValue(event.target.value);
+
+
+      }}
    />
 };
 
-export  {ToDoSearch};
+export { ToDoSearch };
